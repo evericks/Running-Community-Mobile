5bf820529e7bb0e738090df4f461003f1f0b174b
maitanchinh <64420055+maitanchinh@users.noreply.github.com>
2024-04-29 19:09:56 +0700

    Update GroupDetailScreen.dart

--- lib/screens/GroupDetailScreen.dart
+++ lib/screens/GroupDetailScreen.dart
@@ -5,6 +5,7 @@ import 'package:running_community_mobile/cubit/group/group_cubit.dart';
 import 'package:running_community_mobile/cubit/group/group_state.dart';
 import 'package:running_community_mobile/domain/models/groups.dart';
 import 'package:running_community_mobile/domain/repositories/user_repo.dart';
+import 'package:running_community_mobile/screens/PostsScreen.dart';
 import 'package:running_community_mobile/utils/app_assets.dart';
 import 'package:running_community_mobile/utils/colors.dart';
 import 'package:running_community_mobile/utils/gap.dart';
@@ -21,8 +22,7 @@ class GroupDetailScreen extends StatefulWidget {
 }
 
 class _GroupDetailScreenState extends State<GroupDetailScreen> {
-
-    void showLoader(BuildContext context) {
+  void showLoader(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -57,13 +57,16 @@ class _GroupDetailScreenState extends State<GroupDetailScreen> {
                   context.read<GroupCubit>().getGroupById(widget.id);
                 }
                 if (state is JoinGroupFailedState) {
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(state.error.replaceAll('Exception: ', '')), backgroundColor: tomato,));
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(state.error.replaceAll('Exception: ', '')),
+                    backgroundColor: tomato,
+                  ));
                   hideLoader(context);
                   // Navigator.pop(context);
                   context.read<GroupCubit>().getGroupById(widget.id);
                 }
               },
-              builder:(context, state) => BlocBuilder<GroupCubit, GroupState>(builder: (context, state) {
+              builder: (context, state) => BlocBuilder<GroupCubit, GroupState>(builder: (context, state) {
                 if (state is GetGroupLoadingState) {
                   return const Center(
                     child: CircularProgressIndicator(),
@@ -96,39 +99,54 @@ class _GroupDetailScreenState extends State<GroupDetailScreen> {
                                     borderRadius: BorderRadius.circular(8),
                                     child: FadeInImage.assetNetwork(placeholder: AppAssets.placeholder, image: group.thumbnailUrl!, height: 100, width: 100, fit: BoxFit.cover)),
                               ),
-                              UserRepo.user.id == group.groupMembers!.firstWhere((mem) => mem.role == 'Owner').user!.id ? const SizedBox.shrink() :  group.groupMembers!.any((mem) => mem.user!.id == UserRepo.user.id) ? Positioned(
-                                right: 16,
-                                bottom: 0,
-                                  child: Container(
-                                    padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
-                                    decoration: BoxDecoration(
-                                      border: Border.all(color: group.groupMembers!.any((mem) => mem.user!.id == UserRepo.user.id) ? gray : primaryColor),
-                                      color: group.groupMembers!.any((mem) => mem.user!.id == UserRepo.user.id) ? white : primaryColor,
-                                      borderRadius: BorderRadius.circular(8),
-                                    ),
-                                child: Text(group.groupMembers!.any((mem) => mem.user!.id == UserRepo.user.id) ? 'Out' : 'Join', style: boldTextStyle(color: group.groupMembers!.any((mem) => mem.user!.id == UserRepo.user.id) ? grey : white),),
-                              ).onTap((){
-                                if (!group.groupMembers!.any((mem) => mem.user!.id == UserRepo.user.id)) {
-                                  context.read<GroupCubit>().joinGroup(userId: UserRepo.user.id!, groupId: group.id!);
-                                }
-                              })) : Positioned(
-                                right: 16,
-                                bottom: 0,
-                                  child: Container(
-                                    padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
-                                    decoration: BoxDecoration(
-                                      border: Border.all(color: primaryColor),
-                                      color: primaryColor,
-                                      borderRadius: BorderRadius.circular(8),
-                                    ),
-                                child: Text('Join', style: boldTextStyle(color: white),),
-                              ).onTap((){
-                                        if (getStringAsync(AppConstant.TOKEN_KEY) == '') {
-                                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Please login to join the tournament'), backgroundColor: tomato,));
-                                        } else {
-                                          context.read<GroupCubit>().joinGroup(userId: UserRepo.user.id!, groupId: group.id!);
-                                        }
-                                      })),
+                              UserRepo.user.id == group.groupMembers!.firstWhere((mem) => mem.role == 'Owner').user!.id
+                                  ? const SizedBox.shrink()
+                                  : group.groupMembers!.any((mem) => mem.user!.id == UserRepo.user.id)
+                                      ? Positioned(
+                                          right: 16,
+                                          bottom: 0,
+                                          child: Container(
+                                            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+                                            decoration: BoxDecoration(
+                                              border: Border.all(color: group.groupMembers!.any((mem) => mem.user!.id == UserRepo.user.id) ? gray : primaryColor),
+                                              color: group.groupMembers!.any((mem) => mem.user!.id == UserRepo.user.id) ? white : primaryColor,
+                                              borderRadius: BorderRadius.circular(8),
+                                            ),
+                                            child: Text(
+                                              group.groupMembers!.any((mem) => mem.user!.id == UserRepo.user.id) ? 'Out' : 'Join',
+                                              style: boldTextStyle(color: group.groupMembers!.any((mem) => mem.user!.id == UserRepo.user.id) ? grey : white),
+                                            ),
+                                          ).onTap(() {
+                                            if (!group.groupMembers!.any((mem) => mem.user!.id == UserRepo.user.id)) {
+                                              context.read<GroupCubit>().joinGroup(userId: UserRepo.user.id!, groupId: group.id!);
+                                              Navigator.pushReplacementNamed(context, PostsScreen.routeName, arguments: group.id);
+                                            }
+                                          }))
+                                      : Positioned(
+                                          right: 16,
+                                          bottom: 0,
+                                          child: Container(
+                                            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+                                            decoration: BoxDecoration(
+                                              border: Border.all(color: primaryColor),
+                                              color: primaryColor,
+                                              borderRadius: BorderRadius.circular(8),
+                                            ),
+                                            child: Text(
+                                              'Join',
+                                              style: boldTextStyle(color: white),
+                                            ),
+                                          ).onTap(() {
+                                            if (getStringAsync(AppConstant.TOKEN_KEY) == '') {
+                                              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                                                content: Text('Please login to join the tournament'),
+                                                backgroundColor: tomato,
+                                              ));
+                                            } else {
+                                              context.read<GroupCubit>().joinGroup(userId: UserRepo.user.id!, groupId: group.id!);
+                                              Navigator.pushReplacementNamed(context, PostsScreen.routeName, arguments: group.id);
+                                            }
+                                          })),
                             ],
                           ),
                         ),
@@ -136,9 +154,20 @@ class _GroupDetailScreenState extends State<GroupDetailScreen> {
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(group.name!, style: boldTextStyle(size: 24),),
+                            Text(
+                              group.name!,
+                              style: boldTextStyle(size: 24),
+                            ),
                             Gap.k8.height,
-                            Text(group.description!, style: primaryTextStyle(size: 16),),
+                            Text(
+                              group.description!,
+                              style: primaryTextStyle(size: 16),
+                            ),
+                            Gap.k8.height,
+                            Text(
+                              group.rule!,
+                              style: primaryTextStyle(size: 16),
+                            ),
                             Gap.k16.height,
                             Row(
                               children: [
@@ -149,13 +178,11 @@ class _GroupDetailScreenState extends State<GroupDetailScreen> {
                                     borderRadius: BorderRadius.circular(8),
                                   ),
                                   child: RichText(
-                                    textAlign: TextAlign.center,
-                                    text: TextSpan(
-                                    children: [
-                                      TextSpan(text: 'Members', style: secondaryTextStyle(size: 14)),
-                                      TextSpan(text: '  ${group.groupMembers!.length}', style: boldTextStyle(size: 16)),
-                                    ]
-                                  )),
+                                      textAlign: TextAlign.center,
+                                      text: TextSpan(children: [
+                                        TextSpan(text: 'Members', style: secondaryTextStyle(size: 14)),
+                                        TextSpan(text: '  ${group.groupMembers!.length}', style: boldTextStyle(size: 16)),
+                                      ])),
                                 ).expand(),
                                 Gap.k16.width,
                                 Container(
@@ -165,20 +192,20 @@ class _GroupDetailScreenState extends State<GroupDetailScreen> {
                                     borderRadius: BorderRadius.circular(8),
                                   ),
                                   child: RichText(
-                                    textAlign: TextAlign.center,
-                                    text: TextSpan(
-                                    children: [
-                                      TextSpan(text: 'Leader', style: secondaryTextStyle(size: 14)),
-                                      TextSpan(text: '  ${group.groupMembers!.firstWhere((mem) => mem.role == 'Owner').user!.name}', style: boldTextStyle(size: 16)),
-                                    ]
-                                  )),
+                                      textAlign: TextAlign.center,
+                                      text: TextSpan(children: [
+                                        TextSpan(text: 'Leader', style: secondaryTextStyle(size: 14)),
+                                        TextSpan(text: '  ${group.groupMembers!.firstWhere((mem) => mem.role == 'Owner').user!.name}', style: boldTextStyle(size: 16)),
+                                      ])),
                                 ).expand()
                               ],
                             ),
                             Gap.kSection.height,
                             ListView.separated(
-                              shrinkWrap: true,
-                              itemBuilder: (context, index) => MemberWidget(member: group.groupMembers![index]), separatorBuilder:  (context, index) => const Divider(), itemCount: group.groupMembers!.length)
+                                shrinkWrap: true,
+                                itemBuilder: (context, index) => MemberWidget(member: group.groupMembers![index]),
+                                separatorBuilder: (context, index) => const Divider(),
+                                itemCount: group.groupMembers!.length)
                           ],
                         ).paddingSymmetric(horizontal: 16)
                       ],
@@ -203,19 +230,28 @@ class MemberWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-    ClipOval(
-      child: member.user!.avatarUrl == null ? Image.asset(AppAssets.user_placeholder, height: 40, width: 40, fit: BoxFit.cover) : FadeInImage.assetNetwork(placeholder: AppAssets.user_placeholder, image: member.user!.avatarUrl!, height: 40, width: 40, fit: BoxFit.cover),
-    ),
-    Gap.k8.width,
-    Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return Row(
       children: [
-        Text(member.user!.name!, style: boldTextStyle(size: 16),),
-        Text(member.role!, style: secondaryTextStyle(size: 14),),
+        ClipOval(
+          child: member.user!.avatarUrl == null
+              ? Image.asset(AppAssets.user_placeholder, height: 40, width: 40, fit: BoxFit.cover)
+              : FadeInImage.assetNetwork(placeholder: AppAssets.user_placeholder, image: member.user!.avatarUrl!, height: 40, width: 40, fit: BoxFit.cover),
+        ),
+        Gap.k8.width,
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              member.user!.name!,
+              style: boldTextStyle(size: 16),
+            ),
+            Text(
+              member.role!,
+              style: secondaryTextStyle(size: 14),
+            ),
+          ],
+        ).expand()
       ],
-    ).expand()
-                              
-                              ],);
+    );
   }
 }
